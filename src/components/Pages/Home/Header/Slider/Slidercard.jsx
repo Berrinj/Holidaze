54930719d735e42ad323328adaf9fbfc2d94102a
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-01-20 15:16:06 +0100

    Header with intro and venue slider fort the homepage.

---
+++ src/components/Pages/Home/Header/Slider/Slidercard.jsx
@@ -0,0 +1,19 @@
+import { Link } from "react-router-dom";
+
+function SliderCard({ title, img, alt, id }) {
+  return (
+    <Link to={`/venues/${id}`} className="h-full w-full">
+      <img
+        src={img}
+        alt={alt || "Venue Image"}
+        className="slider__img rounded-2xl object-cover h-full w-full "
+      />
+
+      <p className="slider__title rounded-b-2xl w-full flex justify-center items-center  bg-eerieblack w-100 bg-opacity-50 absolute bottom-0 text-white text-4xl min-h-20 overflow-hidden text-center">
+        {title}
+      </p>
+    </Link>
+  );
+}
+
+export default SliderCard;
--- vite.config.js
+++ vite.config.js
@@ -10,6 +10,7 @@ export default defineConfig({
       "@": path.resolve(__dirname, "./src"),
       components: path.resolve(__dirname, "./src/components"),
       assets: path.resolve(__dirname, "./src/assets"),
+      api: path.resolve(__dirname, "./src/api"),
     },
   },
 });
