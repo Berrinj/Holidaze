2b9cdb2de200e5110272aeacdbe29cd8337b8542
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-01-20 21:27:04 +0100

    padding on intro msg

--- src/components/Pages/Home/Header/Intro/index.jsx
+++ src/components/Pages/Home/Header/Intro/index.jsx
@@ -1,13 +1,15 @@
 function Intro() {
   return (
     <div className="flex flex-col justify-center text-white text-center font-extralight flex-1">
-      <p className="text-4xl">WELCOME TO</p>
-      <h1 className="text-6xl mb-6">HOLIDAZE</h1>
-      <p>
-        Your number 1 place to book your next home away from home! Browse over
-        300 holiday homes or list your own in a few simple steps
-      </p>
-      <p className="italic">-The Holidaze Team</p>
+      <div className="p-2">
+        <p className="text-4xl">WELCOME TO</p>
+        <h1 className="text-6xl mb-6">HOLIDAZE</h1>
+        <p>
+          Your number 1 place to book your next home away from home! Browse over
+          300 holiday homes or list your own in a few simple steps
+        </p>
+        <p className="italic">-The Holidaze Team</p>
+      </div>
     </div>
   );
 }
