79bae05ef35185f9d07aa6def4fe530fd08e6482
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-01-20 15:18:26 +0100

    slider title upgrade

--- src/components/Pages/Home/Header/Slider/Slidercard.jsx
+++ src/components/Pages/Home/Header/Slider/Slidercard.jsx
@@ -9,7 +9,7 @@ function SliderCard({ title, img, alt, id }) {
         className="slider__img rounded-2xl object-cover h-full w-full "
       />
 
-      <p className="slider__title rounded-b-2xl w-full flex justify-center items-center  bg-eerieblack w-100 bg-opacity-50 absolute bottom-0 text-white text-4xl min-h-20 overflow-hidden text-center">
+      <p className="slider__title rounded-b-2xl w-full flex justify-center items-center  bg-eerieblack w-100 bg-opacity-60 absolute bottom-0 text-white text-4xl min-h-20 overflow-hidden text-center font-light">
         {title}
       </p>
     </Link>
