e50289aa225d112f1de851ee74af7d4d420a9d34
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-02-03 15:03:10 +0100

    fixed image display when image is small

--- src/components/Modals/ImageModal/index.jsx
+++ src/components/Modals/ImageModal/index.jsx
@@ -3,7 +3,7 @@ import { Modal } from "components/Modals/Modal";
 const ImageModal = ({ isOpen, onClose, data }) => {
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
-      <div className="image-modal">
+      <div className="image-modal flex justify-center">
         <img src={data.avatar.url} alt={data.avatar.alt} />
       </div>
     </Modal>
