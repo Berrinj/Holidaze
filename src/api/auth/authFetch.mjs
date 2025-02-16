7497513e132d09f68e7d00c99555fe551591c12d
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-02-16 09:58:44 +0100

    API key issue on build troubleshoot

--- src/api/auth/authFetch.mjs
+++ src/api/auth/authFetch.mjs
@@ -1,8 +1,21 @@
 import { headers } from "../headers.mjs";
 
+// export async function authFetch(url, options = {}) {
+//   return fetch(url, {
+//     ...options,
+//     headers: headers(Boolean(options.body)),
+//   });
+// }
 export async function authFetch(url, options = {}) {
+  const requestHeaders = headers(Boolean(options.body));
+
+  // Log the headers to the console
+  for (let pair of requestHeaders.entries()) {
+    console.log(`${pair[0]}: ${pair[1]}`);
+  }
+
   return fetch(url, {
     ...options,
-    headers: headers(Boolean(options.body)),
+    headers: requestHeaders,
   });
 }
