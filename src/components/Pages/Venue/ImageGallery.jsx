70fa77801cb72ee1a32fbca0c121d670b6fd8dcf
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-02-16 11:52:01 +0100

    added default image if no media

--- src/components/Pages/Venue/ImageGallery.jsx
+++ src/components/Pages/Venue/ImageGallery.jsx
@@ -3,6 +3,19 @@ import "react-image-gallery/styles/css/image-gallery.css";
 import "./ImageGallery.css";
 
 export const VenueImageGallery = ({ media }) => {
+  const defaultImage =
+    "https://images.unsplash.com/photo-1634712282287-14ed57b9cc89?q=80&w=1206&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D";
+
+  if (!media || media.length === 0) {
+    return (
+      <img
+        src={defaultImage}
+        alt="Default venue"
+        className="object-cover w-full rounded-t-2xl"
+      />
+    );
+  }
+
   const galleryImages = media.map((image) => ({
     original: image.url,
     originalAlt: image.alt,
