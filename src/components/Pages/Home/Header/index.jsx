d813dac0c63e339a5f203c36232381dd53256cc5
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-02-16 12:25:05 +0100

    header responsiveness update

--- src/components/Pages/Home/Header/index.jsx
+++ src/components/Pages/Home/Header/index.jsx
@@ -3,7 +3,7 @@ import Slider from "./Slider";
 
 function HomeHeader() {
   return (
-    <div className="home-header bg-brass bg-opacity-70 flex rounded-2xl min-h-80">
+    <div className="home-header bg-brass bg-opacity-70 flex flex-wrap rounded-2xl min-h-80">
       <Intro />
       <Slider />
     </div>
