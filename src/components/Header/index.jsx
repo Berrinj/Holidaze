07d9ff3d0d825e14af3fa44a1b525ff43d85ee9f
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-02-17 20:37:22 +0100

    logo based on screen width

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom";
 import headerlogo from "assets/headerlogo.png";
+import hlogo from "assets/hlogo.png";
 import Nav from "./Nav/Nav";
 
 function Header() {
@@ -7,7 +8,16 @@ function Header() {
     <header className="border-b border-mineshaft ">
       <div className="header__logo-nav text-white h-16 p-0 flex justify-between items-center max-w-screen-xl mx-auto px-3">
         <Link to="/">
-          <img src={headerlogo} alt="Holidaze Logo" className="logo" />
+          <img
+            src={headerlogo}
+            alt="Holidaze Logo"
+            className="hidden sm:block logo"
+          />
+          <img
+            src={hlogo}
+            alt="Holidaze Logo"
+            className="block sm:hidden logo"
+          />
         </Link>
         <Nav />
       </div>
