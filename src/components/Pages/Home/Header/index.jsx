c23e0c9c12438c899b370d871280fe55727a9f1b
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-02-20 11:48:06 +0100

    intro only renders if user is not signed in

--- src/components/Pages/Home/Header/index.jsx
+++ src/components/Pages/Home/Header/index.jsx
@@ -1,10 +1,12 @@
 import Intro from "./Intro";
 import Slider from "./Slider";
+import { load } from "utils/localStorage.mjs";
 
 function HomeHeader() {
+  const userStatus = load("profile");
   return (
     <div className="home-header bg-brass bg-opacity-70 flex flex-wrap rounded-2xl min-h-80">
-      <Intro />
+      {!userStatus && <Intro />}
       <Slider />
     </div>
   );
