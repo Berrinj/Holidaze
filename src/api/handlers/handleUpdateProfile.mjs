7a1b1ba8d6287dde8bdd699f3e5afdb4d7193d5e
Berrinj <69366646+Berrinj@users.noreply.github.com>
2025-02-22 12:33:26 +0100

    form validation with yup + react-hook-form

--- src/api/handlers/handleUpdateProfile.mjs
+++ src/api/handlers/handleUpdateProfile.mjs
@@ -2,46 +2,35 @@ import UpdateData from "api/data/update";
 import { PROFILES_URL } from "api/constants.mjs";
 
 /**
- * This function handles updating a profile, takes the event and id as arguments, id being the profile name.
- * @param {*} event - the event object
- * @param {string} id the profile name
- * @returns the result of the update, either success or failure
+ * This function handles updating a profile, takes the form data and id as arguments, id being the profile name.
+ * @param {Object} data - The form data
+ * @param {string} id - The profile name
+ * @returns {Promise<Object>} The result of the update, either success or failure
  */
 
-const handleUpdateProfile = async (event, id) => {
-  event.preventDefault();
-  const formData = new FormData(event.target);
+const handleUpdateProfile = async (data, id) => {
   const profile = {
-    bio: formData.get("bio"),
-    venueManager: formData.get("venueManager") === "venue-manager",
+    bio: data.bio,
+    venueManager: data.venueManager === "venue-manager",
   };
 
-  const avatarUrl = formData.get("avatar");
-  const avatarAlt = formData.get("avatarAlt");
-  if (avatarUrl) {
+  if (data.avatar) {
     profile.avatar = {
-      url: avatarUrl,
-      alt: avatarAlt || "",
+      url: data.avatar,
+      alt: data.avatarAlt || "",
     };
   }
 
-  const bannerUrl = formData.get("banner");
-  const bannerAlt = formData.get("bannerAlt");
-  if (bannerUrl) {
+  if (data.banner) {
     profile.banner = {
-      url: bannerUrl,
-      alt: bannerAlt || "",
+      url: data.banner,
+      alt: data.bannerAlt || "",
     };
   }
 
   try {
     const result = await UpdateData(PROFILES_URL, id, profile);
     console.log("Result from UpdateData:", result);
-    // if (result.status === 200) {
-    //   console.log("Update successful");
-    // } else {
-    //   console.error("Update failed");
-    // }
     return result;
   } catch (error) {
     console.error("Update failed:", error);
@@ -50,33 +39,3 @@ const handleUpdateProfile = async (event, id) => {
 };
 
 export default handleUpdateProfile;
-
-// const handleUpdateProfile = async (event, formData, data) => {
-//   event.preventDefault();
-//   const payload = {
-//     bio: formData.bio,
-//     avatar: {
-//       url: formData.avatar,
-//       alt: formData.avatarAlt,
-//     },
-//     banner: {
-//       url: formData.banner,
-//       alt: formData.bannerAlt,
-//     },
-//     venueManager: formData.venueManager,
-//   };
-//   try {
-//     const result = await UpdateData(PROFILES_URL, data.name, payload);
-//     console.log(result);
-//     if (result.status === 200) {
-//       console.log("Update successful");
-//     } else {
-//       console.error("Update failed");
-//     }
-//   } catch (error) {
-//     console.error("Update failed:", error);
-//     return { errors: [{ message: error.message }] };
-//   }
-// };
-
-// export default handleUpdateProfile;
